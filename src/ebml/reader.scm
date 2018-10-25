86083deb8a5cc79e53e3330e19626a8155532043
Steve Erhart <steve.erhart@dudesolutions.com>
2018-10-25 18:49:19 -0400

    starting

--- src/ebml/reader.scm
+++ src/ebml/reader.scm
@@ -1,5 +1,8 @@
 (define-module (ebml reader)
-  #:use-module (rnrs bytevectors))
+  #:use-module (rnrs bytevectors)
+  #:use-module (rnrs arithmetic bitwise)
+  #:use-module (ice-9 binary-ports)
+  #:use-module (ice-9 receive))
 
 (define (ebml-read in)
   (cond [(bytevector? in)
@@ -8,3 +11,17 @@
          'port]
         [else
          (error "don't know how to open")]))
+
+
+(define (parse-elements port)
+  (cond [(eof-object? (lookahead-u8 port))]
+        [else
+         (cons (parse-element port)
+               (parse-elements port))]))
+
+(define (parse-element port)
+  (define first-byte (get-u8 port))
+
+  ;(receive (cond
+  ;          [(not (= 0 (bitwise-and #x80 first-byte)))]))
+  port)
